e5b6e4c599434227e5f15e31a04ffd12f811adcd
Denethor03 <szych.mateusz2@gmail.com>
2025-10-17 15:15:21 +0200

    test

--- Zadania_2/pararell_sort.py
+++ Zadania_2/pararell_sort.py
@@ -5,7 +5,7 @@ def quicksort(array):
     if len(array) <= 1:
         return array
     pivot = array[-1]
-    left = [x for x in array if x < pivot]
+    left = [x for x in array if x < pivot] #test comment
     mid = [x for x in array if x == pivot]
     right = [x for x in array if x > pivot]
     return quicksort(left) + mid + quicksort(right)
